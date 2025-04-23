7005d584be5d6c1e5326f9c09ebd2b67621b6b71
Witchman18 <103100213+Witchman18@users.noreply.github.com>
2025-04-23 23:36:53 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -135,17 +135,17 @@ if __name__ == "__main__":
 
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.Regex("📊 Топ 5 funding-пар"), show_top_funding))
-    app.add_handler(MessageHandler(filters.Regex("📈 Расчёт прибыли"), start))
-    app.add_handler(MessageHandler(filters.Regex("📡 Сигналы"), start))
+    app.add_handler(MessageHandler(filters.Regex("📈 Расчёт прибыли"), start_calc))
+    app.add_handler(MessageHandler(filters.Regex("📡 Сигналы"), signal_menu))
     app.add_handler(MessageHandler(filters.Regex("🔧 Установить маржу"), set_real_marja))
     app.add_handler(CallbackQueryHandler(start))
 
-    conv_handler = ConversationHandler(
-        entry_points=[MessageHandler(filters.Regex("📈 Расчёт прибыли"), start)],
-        states={
-            MARJA: [MessageHandler(filters.TEXT & ~filters.COMMAND, start)],
-            PLECHO: [MessageHandler(filters.TEXT & ~filters.COMMAND, start)],
-        },
+   conv_handler = ConversationHandler(
+    entry_points=[MessageHandler(filters.Regex("📈 Расчёт прибыли"), start_calc)],
+    states={
+        MARJA: [MessageHandler(filters.TEXT & ~filters.COMMAND, set_marja)],
+        PLECHO: [MessageHandler(filters.TEXT & ~filters.COMMAND, set_plecho)],
+    },    
         fallbacks=[CommandHandler("cancel", cancel)],
     )
     app.add_handler(conv_handler)
